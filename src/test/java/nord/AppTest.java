a91e6fb5ddbc00cf261addfadbae4b3c71501717
Erik Nord <tpi13eno@student.lu.se>
2020-12-12 16:31:16 +0100

    added stall test, to check hooks

--- src/test/java/nord/AppTest.java
+++ src/test/java/nord/AppTest.java
@@ -2,6 +2,7 @@ package nord;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,5 +12,14 @@ public class AppTest{
     @Test
     public void shouldAnswerWithTrue(){
         assertTrue(true);
+    }
+
+    @Test
+    public void stall() {
+        for(int i = 0; i < 99999999; i++) {
+            double random = Math.random();
+            random = random / 2;
+            Assert.assertTrue("true", random >= 0);
+        }
     }
 }
